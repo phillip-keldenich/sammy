135ee61407a1552289d226ed6ed2b056f6bc108a
Michael Perk <perk@ibr.cs.tu-bs.de>
2025-06-29 12:43:45 +0900

    exp: yasa 1h for all instances.

--- experiments/04_baseline_1h/01_run_yasa.py
+++ experiments/04_baseline_1h/01_run_yasa.py
@@ -20,9 +20,55 @@ slurminade.set_dispatch_limit(300)
 instances_path = "../../sammy_benchmark_instances"
 output_dir = "yasa_1h"
 instances = [
-    # "ChatClient",
-    # "DMIE",
-    # "E-Shop",
+    # 46 instances from SampLNS paper
+    "busybox-1_29_2",
+    "busybox_2007-01-24_09-14-09",
+    "busybox_2020-12-16_21-53-05",
+    "fiasco_2017-09-26_11-30-56",
+    "fiasco_2020-12-01_14-09-14",
+    "soletta_2015-06-26_18-38-56",
+    "soletta_2017-03-09_21-02-40",
+    "toybox_2006-10-31_23-30-06",
+    "toybox_2020-12-06_00-02-46",
+    "uclibc_2008-06-05_13-46-47",
+    "uclibc_2020-12-24_11-54-53",
+    "APL-Model",
+    "APL",
+    "BankingSoftware",
+    "BattleofTanks",
+    "ChatClient",
+    "DMIE",
+    "E-Shop",
+    "EMBToolkit",
+    "FameDB",
+    "FeatureIDE",
+    "FreeBSD-8_0_0",
+    "PPU",
+    "SafeBali",
+    "SortingLine",
+    "TightVNC",
+    "Violet",
+    "WaterlooGenerated",
+    "XSEngine",
+    "aaed2000",
+    "am31_sim",
+    "atlas_mips32_4kc",
+    "axTLS",
+    "berkeleyDB1",
+    "berkeleyDB2",
+    "busybox-1_18_0",
+    "calculate",
+    "car",
+    "dell",
+    "eCos-3-0_i386pc",
+    "ea2468",
+    "email",
+    "financial_services",
+    "fs_2017-05-22",
+    "gpl",
+    "integrator_arm7",
+    "lcm"
+    # 8 new instances of larger size
     "Automotive01",
     "Automotive02_V1",
     "Automotive02_V2",
@@ -33,6 +79,7 @@ instances = [
     "linux_2_6_33_3"
 ]
 time_limit = 3600  # seconds
+n_repeats = 5
 
 benchmark = Benchmark(output_dir, hide_output=False)
 
@@ -121,7 +168,7 @@ if __name__ == "__main__":
             msg
         )
 
-    for i in range(5):
+    for i in range(n_repeats):
         for instance in instances:
             solve_yasa.distribute(instance_name=instance, alg_params={
                 "algorithm": "yasa",
